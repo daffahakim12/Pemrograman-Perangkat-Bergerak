3a11264db29c8c6270faad1941af0d732c13e921
daffahakim12 <daffahakim028@gmail.com>
2025-10-20 10:54:33 +0700

    lib prak 4

---
+++ Praktikum/Praktikum 4/lib/components/navbar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home.dart';
+import 'package:flutter_application_1/pages/search.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int _selectedIndex = 0;
+
+  void _changeIndex(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Navbar Example'),
+        backgroundColor: Colors.blue,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomePage(),
+          SearchPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _changeIndex,
+        selectedItemColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
